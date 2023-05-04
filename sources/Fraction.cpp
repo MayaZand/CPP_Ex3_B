e969c6882d50ba4026940804bfa34205f9188c81
MayaZand <mayaz96101@gmail.com>
2023-05-04 12:28:07 +0300

    final commit - passed all tests

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -2,237 +2,218 @@
 
 using namespace ariel;
 
-Fraction:: Fraction(): numerator(0), denominator(1){};
-Fraction:: Fraction(int numerator, int denominator)
-{   
-    
-    if (denominator == 0)
-    {
-        throw invalid_argument ("denominator can't be 0!");
-    }
+Fraction::Fraction() : numerator(0), denominator(1){};
+Fraction::Fraction(int numerator, int denominator)
+{
 
-    
-    int gcd_num = __gcd(numerator, denominator);
-    numerator /= gcd_num;
-    denominator /= gcd_num;
+    if (denominator == 0) // denominator can't be zero
+    {
+        throw invalid_argument("denominator can't be 0!");
+    }
 
     this->numerator = numerator;
     this->denominator = denominator;
 
-
-    if (this->denominator < 0)
+    if (this->denominator < 0) // if the Fraction is negative - make numerator negative and denominator positive.
     {
-        this->numerator*=-1;
-        this->denominator*=-1;
+        this->numerator *= -1;
+        this->denominator *= -1;
     }
+    
+    reduceFrac();
 }
 
-Fraction:: Fraction(float number) 
+Fraction::Fraction(float number)
 {
     int numerator = (number * 1000);
     int denominator = 1000;
-    int gcd_num = __gcd(numerator, denominator);
-    numerator /= gcd_num;
-    denominator /= gcd_num;
 
     this->numerator = numerator;
     this->denominator = denominator;
 
     if (this->denominator < 0)
     {
-        this->numerator*=-1;
-        this->denominator*=-1;
+        this->numerator *= -1;
+        this->denominator *= -1;
     }
+    
+    reduceFrac();
 }
 
-// float Fraction::fracToFloat()
-// {
-//     float num = this->getNumerator() / this->getDenominator();
+void Fraction::reduceFrac()
+{
+    int gcd = std::gcd(this->numerator, this->denominator);
+    this->numerator = this->numerator / gcd;
+    this->denominator = this->denominator / gcd;
+}
 
-//     return round(num*1000)/1000;
-// }
-
-int Fraction:: getNumerator() const
+int Fraction::getNumerator() const
 {
     return this->numerator;
 }
 
-int Fraction:: getDenominator() const
+int Fraction::getDenominator() const
 {
     return this->denominator;
 }
 
-Fraction ariel:: operator+(const Fraction& fraction1, const Fraction& fraction2)
-{   
+Fraction ariel::operator+(const Fraction &fraction1, const Fraction &fraction2)
+{
     long num = static_cast<long>(fraction1.getNumerator()) * fraction2.getDenominator() + static_cast<long>(fraction2.getNumerator()) * fraction1.getDenominator();
     long denom = static_cast<long>(fraction1.getDenominator()) * fraction2.getDenominator();
 
     int max_int = std::numeric_limits<int>::max();
     int min_int = std::numeric_limits<int>::min();
 
-    if (num>max_int || num<min_int || denom>max_int || denom<min_int)
+    if (num > max_int || num < min_int || denom > max_int || denom < min_int)
     {
-        throw overflow_error ("overflow error!");
+        throw overflow_error("overflow error!");
     }
-    // int num = fraction1.getNumerator() * fraction2.getDenominator() + fraction2.getNumerator() * fraction1.getDenominator();
-    // int denom = fraction1.getDenominator() * fraction2.getDenominator();
-    int gcd = std::gcd(num, denom);
 
-    return Fraction(num/gcd, denom/gcd); 
+    return Fraction(num, denom);
 }
 
-Fraction ariel:: operator-(const Fraction& fraction1, const Fraction& fraction2)
+Fraction ariel::operator-(const Fraction &fraction1, const Fraction &fraction2)
 {
     long num = static_cast<long>(fraction1.getNumerator()) * fraction2.getDenominator() - static_cast<long>(fraction2.getNumerator()) * fraction1.getDenominator();
     long denom = static_cast<long>(fraction1.getDenominator()) * fraction2.getDenominator();
-    
 
     int max_int = std::numeric_limits<int>::max();
     int min_int = std::numeric_limits<int>::min();
 
-    if (num>max_int || num<min_int || denom>max_int || denom<min_int)
+    if (num > max_int || num < min_int || denom > max_int || denom < min_int)
     {
-        throw overflow_error ("overflow error!");
+        throw overflow_error("overflow error!");
     }
-    // int num = fraction1.getNumerator() * fraction2.getDenominator() - fraction2.getNumerator() * fraction1.getDenominator();
-    // int denom = fraction1.getDenominator() * fraction2.getDenominator();
-    int gcd = std::gcd(num, denom);
 
-    return Fraction(num/gcd, denom/gcd); 
+    return Fraction(num, denom);
 }
 
-Fraction ariel:: operator*(const Fraction& fraction1, const Fraction& fraction2)
+Fraction ariel::operator*(const Fraction &fraction1, const Fraction &fraction2)
 {
     long num = static_cast<long>(fraction1.getNumerator()) * fraction2.getNumerator();
     long denom = static_cast<long>(fraction1.getDenominator()) * fraction2.getDenominator();
-    
+
     int max_int = std::numeric_limits<int>::max();
     int min_int = std::numeric_limits<int>::min();
 
-    if (num>max_int || num<min_int || denom>max_int || denom<min_int)
+    if (num > max_int || num < min_int || denom > max_int || denom < min_int)
     {
-        throw overflow_error ("overflow error!");
+        throw overflow_error("overflow error!");
     }
-    // int num = fraction1.getNumerator() * fraction2.getNumerator();
-    // int denom = fraction1.getDenominator() * fraction2.getDenominator();
-    int gcd = std::gcd(num, denom);
 
-    return Fraction(num/gcd, denom/gcd); 
-
+    return Fraction(num, denom);
 }
 
-Fraction ariel:: operator/(const Fraction& fraction1, const Fraction& fraction2)
+Fraction ariel::operator/(const Fraction &fraction1, const Fraction &fraction2)
 {
-    if ( fraction2.getNumerator() == 0) 
+    if (fraction2.getNumerator() == 0)
     {
-        throw runtime_error("Illegal to divide by 0");
+        throw runtime_error("illegal to divide by 0");
     }
 
     long num = static_cast<long>(fraction1.getNumerator()) * fraction2.getDenominator();
     long denom = static_cast<long>(fraction1.getDenominator()) * fraction2.getNumerator();
-    
+
     int max_int = std::numeric_limits<int>::max();
     int min_int = std::numeric_limits<int>::min();
 
-    if (num>max_int || num<min_int || denom>max_int || denom<min_int)
+    if (num > max_int || num < min_int || denom > max_int || denom < min_int)
     {
-        throw overflow_error ("overflow error!");
+        throw overflow_error("overflow error!");
     }
-    // int num = fraction1.getNumerator() * fraction2.getDenominator();
-    // int denom = fraction1.getDenominator() * fraction2.getNumerator();
-    int gcd = std::gcd(num, denom);
 
-    return Fraction(num/gcd, denom/gcd); 
+    return Fraction(num, denom);
 }
 
-bool ariel:: operator==(const Fraction& fraction1, const Fraction& fraction2)
+bool ariel::operator==(const Fraction &fraction1, const Fraction &fraction2)
 {
 
     return fraction1.getNumerator() * fraction2.getDenominator() == fraction2.getNumerator() * fraction1.getDenominator();
 }
 
-bool ariel:: operator<(const Fraction& fraction1, const Fraction& fraction2)
+bool ariel::operator<(const Fraction &fraction1, const Fraction &fraction2)
 {
     return fraction1.getNumerator() * fraction2.getDenominator() < fraction2.getNumerator() * fraction1.getDenominator();
 }
 
-bool ariel:: operator>(const Fraction& fraction1, const Fraction& fraction2)
+bool ariel::operator>(const Fraction &fraction1, const Fraction &fraction2)
 {
-    return fraction1.getNumerator() * fraction2.getDenominator() > fraction2.getNumerator() * fraction1.getDenominator(); 
+    return fraction1.getNumerator() * fraction2.getDenominator() > fraction2.getNumerator() * fraction1.getDenominator();
 }
 
-bool ariel::  operator<=(const Fraction& fraction1, const Fraction& fraction2)
+bool ariel::operator<=(const Fraction &fraction1, const Fraction &fraction2)
 {
-    return fraction1.getNumerator() * fraction2.getDenominator() <= fraction2.getNumerator() * fraction1.getDenominator(); 
+    return fraction1.getNumerator() * fraction2.getDenominator() <= fraction2.getNumerator() * fraction1.getDenominator();
 }
 
-bool ariel:: operator>=(const Fraction& fraction1, const Fraction& fraction2)
+bool ariel::operator>=(const Fraction &fraction1, const Fraction &fraction2)
 {
-    return fraction1.getNumerator() * fraction2.getDenominator() >= fraction2.getNumerator() * fraction1.getDenominator(); 
+    return fraction1.getNumerator() * fraction2.getDenominator() >= fraction2.getNumerator() * fraction1.getDenominator();
 }
 
-Fraction& Fraction:: operator++() 
+Fraction &Fraction::operator++()
 {
     this->numerator += this->denominator;
-    int gcd = std::gcd(this->numerator, this->denominator);
+    reduceFrac();
 
-    this->numerator = this->numerator/gcd;
-    this->denominator = this->denominator/gcd;
-    
     return *this;
 }
 
-Fraction Fraction:: operator++(int) 
+Fraction Fraction::operator++(int)
 {
     Fraction temp(*this);
     ++(*this);
-    
+
     return temp;
 }
 
-Fraction& Fraction::  operator--() 
+Fraction &Fraction::operator--()
 {
     this->numerator -= this->denominator;
-    int gcd = std::gcd(this->numerator, this->denominator);
-
-    this->numerator = this->numerator/gcd;
-    this->denominator = this->denominator/gcd;
+    reduceFrac();
     
     return *this;
 }
 
-Fraction Fraction:: operator--(int) 
+Fraction Fraction::operator--(int)
 {
     Fraction temp(*this);
     --(*this);
-    
+
     return temp;
 }
 
-ostream& ariel:: operator<<(ostream& ostream, const Fraction& other)
+ostream &ariel::operator<<(ostream &ostream, const Fraction &other)
 {
     return (ostream << other.getNumerator() << '/' << other.getDenominator());
 }
 
-istream& ariel:: operator>>(istream& istream, Fraction& other) 
+istream &ariel::operator>>(istream &istream, Fraction &other)
 {
     int numerator, denominator;
 
     istream >> numerator >> denominator;
-    
+
     if (denominator == 0)
     {
-        throw runtime_error ("denominator can't be zero");
+        throw runtime_error("denominator can't be zero");
     }
 
     if (istream.fail() == true)
     {
-        throw runtime_error ("stream must include two arguments!");
+        throw runtime_error("stream must include two arguments!");
+    }
+
+    if (denominator < 0) // if the Fraction is negative - make numerator negative and denominator positive.
+    {
+        numerator *= -1;
+        denominator *= -1;
     }
 
     other.numerator = numerator;
     other.denominator = denominator;
+    other.reduceFrac();
 
     return istream;
 }
-
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -1,7 +1,9 @@
-/* During the preperation of this assignment I used the internet and chat GPT */
+/*
+During the preperation of this assignment I used the internet and chat GPT, and got help from Roy Simanovich and Ofir Shitrit.
+*/
 #pragma once
 #include <iostream>
-#include <numeric> 
+#include <numeric>
 #include <cmath>
 
 using namespace std;
@@ -10,142 +12,139 @@ namespace ariel
    class Fraction
    {
 
-    /*fields - private by default: */
-    int numerator;
-    int denominator;
+      /*fields - private by default: */
+      int numerator;
+      int denominator;
 
-    /*constructors, copy constructor and destructor: */
-    public:
-    Fraction(); // default constructor 
-    Fraction(int numerator, int denominator);
-    Fraction(float number); // this constructor will convert float into a Fraction
+      /*constructors: */
+      public:
+      Fraction(); // default constructor
+      Fraction(int numerator, int denominator);
+      Fraction(float number); // this constructor will convert float into a Fraction
 
-    /*methods: */
+      /*methods: */
 
-    /*getters: */
+      /*getters: */
 
-    /*
-    @return the numerator of the Fraction.
-    */
-    int getNumerator() const;
-    
-    /*
-    @return the denominator of the Fraction.
-    */
-    int getDenominator() const;
+      /*
+      @return the numerator of the Fraction.
+      */
+      int getNumerator() const;
 
-    /*
-    this methods add two fractions and return their sum as another fraction in reduced form. 
-    @param fraction1 is the first fraction, on the left side of the operator.
-    @param fraction2 is the second fraction, on the right side of the operator.
-    @return the sum of two fractions.
-    */
+      /*
+      @return the denominator of the Fraction.
+      */
+      int getDenominator() const;
 
-   friend Fraction operator+(const Fraction& fraction1, const Fraction& fraction2);
+      /*
+      this method reduces the Fraction.
+      */
+      void reduceFrac();
 
-    /*
-    this method subtract two fractions and return their difference as another fraction in reduced form.
-    @param fraction1 is the first fraction, on the left side of the operator.
-    @param fraction2 is the second fraction, on the right side of the operator.
-    @return the subtraction of two fractions.
-    */
-   friend Fraction operator-(const Fraction& fraction1, const Fraction& fraction2);
+      /*
+      this method add two fractions and return their sum as another fraction in reduced form.
+      @param fraction1 is the first fraction, on the left side of the operator.
+      @param fraction2 is the second fraction, on the right side of the operator.
+      @return the sum of two fractions.
+      */
+      friend Fraction operator+(const Fraction &fraction1, const Fraction &fraction2);
 
-    /*
-    this method multiply two fractions and return their product as another fraction in reduced form.
-    @param fraction1 is the first fraction, on the left side of the operator.
-    @param fraction2 is the second fraction, on the right side of the operator.
-    @return the multiplication of two fractions.
-    */
-   friend Fraction operator*(const Fraction& fraction1, const Fraction& fraction2);
+      /*
+      this method subtract two fractions and return their difference as another fraction in reduced form.
+      @param fraction1 is the first fraction, on the left side of the operator.
+      @param fraction2 is the second fraction, on the right side of the operator.
+      @return the subtraction of two fractions.
+      */
+      friend Fraction operator-(const Fraction &fraction1, const Fraction &fraction2);
 
-    /*
-    this method divide two fractions and return their quotient as another fraction in reduced form.
-    @param fraction1 is the first fraction, on the left side of the operator.
-    @param fraction2 is the second fraction, on the right side of the operator.
-    @return the division of two fractions.
-    */
-   friend Fraction operator/(const Fraction& fraction1, const Fraction& fraction2);
+      /*
+      this method multiply two fractions and return their product as another fraction in reduced form.
+      @param fraction1 is the first fraction, on the left side of the operator.
+      @param fraction2 is the second fraction, on the right side of the operator.
+      @return the multiplication of two fractions.
+      */
+      friend Fraction operator*(const Fraction &fraction1, const Fraction &fraction2);
 
-    /*
-    this method compare two fractions for equality and return true or false.
-    @param fraction1 is the first fraction, on the left side of the operator.
-    @param fraction2 is the second fraction, on the right side of the operator.
-    @return true if two fractions are equal, and false otherwise.
-    */
-   friend bool operator==(const Fraction& fraction1, const Fraction& fraction2);
+      /*
+      this method divide two fractions and return their quotient as another fraction in reduced form.
+      @param fraction1 is the first fraction, on the left side of the operator.
+      @param fraction2 is the second fraction, on the right side of the operator.
+      @return the division of two fractions.
+      */
+      friend Fraction operator/(const Fraction &fraction1, const Fraction &fraction2);
 
+      /*
+      this method compare two fractions for equality and return true or false.
+      @param fraction1 is the first fraction, on the left side of the operator.
+      @param fraction2 is the second fraction, on the right side of the operator.
+      @return true if two fractions are equal, and false otherwise.
+      */
+      friend bool operator==(const Fraction &fraction1, const Fraction &fraction2);
 
-    /*
-    this method compare two products and return true if the first product is less than the second, and false otherwise.
-    @param fraction1 is the first fraction, on the left side of the operator.
-    @param fraction2 is the second fraction, on the right side of the operator.
-    @return true if fraction1 is smaller than fraction2, and false otherwise.
-    */
-   friend bool operator<(const Fraction& fraction1, const Fraction& fraction2);
+      /*
+      this method compare two products and return true if the first product is less than the second, and false otherwise.
+      @param fraction1 is the first fraction, on the left side of the operator.
+      @param fraction2 is the second fraction, on the right side of the operator.
+      @return true if fraction1 is smaller than fraction2, and false otherwise.
+      */
+      friend bool operator<(const Fraction &fraction1, const Fraction &fraction2);
 
+      /*
+      this method compare two products and return true if the second product is less than the first, and false otherwise.
+      @param fraction1 is the first fraction, on the left side of the operator.
+      @param fraction2 is the second fraction, on the right side of the operator.
+      @return true if fraction1 is bigger than fraction2, and false otherwise.
+      */
+      friend bool operator>(const Fraction &fraction1, const Fraction &fraction2);
 
-    /*
-    this method compare two products and return true if the second product is less than the first, and false otherwise.
-    @param fraction1 is the first fraction, on the left side of the operator.
-    @param fraction2 is the second fraction, on the right side of the operator.
-    @return true if fraction1 is bigger than fraction2, and false otherwise.
-    */
-    friend bool operator>(const Fraction& fraction1, const Fraction& fraction2);
+      /*
+      this method compare two products and return true if the first product is less or equal to the second, and false otherwise.
+      @param fraction1 is the first fraction, on the left side of the operator.
+      @param fraction2 is the second fraction, on the right side of the operator.
+      @return true if fraction1 is smaller or equal to fraction2, and false otherwise.
+      */
+      friend bool operator<=(const Fraction &fraction1, const Fraction &fraction2);
 
+      /*
+      this method compare two products and return true if the second product is less or equal to the first, and false otherwise.
+      @param fraction1 is the first fraction, on the left side of the operator.
+      @param fraction2 is the second fraction, on the right side of the operator.
+      @return true if fraction1 is bigger or equal to fraction2, and false otherwise.
+      */
+      friend bool operator>=(const Fraction &fraction1, const Fraction &fraction2);
 
-    /*
-    this method compare two products and return true if the first product is less or equal to the second, and false otherwise.
-    @param fraction1 is the first fraction, on the left side of the operator.
-    @param fraction2 is the second fraction, on the right side of the operator.
-    @return true if fraction1 is smaller or equal to fraction2, and false otherwise.
-    */
-    friend bool operator<=(const Fraction& fraction1, const Fraction& fraction2);
+      /*
+      this method adds 1 to the fraction - prefix implementation
+      */
+      Fraction &operator++();
 
+      /*
+      this method adds 1 to the fraction - postfix implementation
+      */
+      Fraction operator++(int);
 
-    /*
-    this method compare two products and return true if the second product is less or equal to the first, and false otherwise.
-    @param fraction1 is the first fraction, on the left side of the operator.
-    @param fraction2 is the second fraction, on the right side of the operator.
-    @return true if fraction1 is bigger or equal to fraction2, and false otherwise.
-    */
-    friend bool operator>=(const Fraction& fraction1, const Fraction& fraction2);
+      /*
+      this method substracts 1 from the fraction - prefix implementation
+      */
+      Fraction &operator--();
 
+      /*
+      this method substracts 1 from the fraction - postfix implementation
+      */
+      Fraction operator--(int);
 
-    /*
-    this method adds 1 to the fraction - prefix implementation
-    */
-    Fraction& operator++();
-        
-    /*
-    this method adds 1 to the fraction - postfix implementation
-    */
-    Fraction operator++(int);
+      /*
+      this method prints a fraction to an output stream in the format “numerator/denominator”.
+      @param ostream is the output stream.
+      */
+      friend ostream &operator<<(ostream &ostream, const Fraction &other);
 
-    /*
-    this method substracts 1 from the fraction - prefix implementation
-    */
-    Fraction& operator--();
-
-    /*
-    this method substracts 1 from the fraction - postfix implementation
-    */
-    Fraction operator--(int);
-
-    /*
-    this method prints a fraction to an output stream in the format “numerator/denominator”.
-    @param ostream is the output stream
-    */
-    friend ostream& operator<<(ostream& ostream, const Fraction& other);
-
-    /*
-    this method reads a fraction from an input stream by taking two integers as input.
-    @param istream is the input stream
-    */
-    friend istream& operator>>(std::istream& istream, Fraction& other);
-
-    // float fracToFloat();
-   }; 
+      /*
+      this method reads a fraction from an input stream by taking two integers as input.
+      @param istream is the input stream
+      */
+      friend istream &operator>>(std::istream &istream, Fraction &other);
+      
+   };
 
 }
-
